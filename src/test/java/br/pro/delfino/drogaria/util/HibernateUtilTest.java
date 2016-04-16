2059962cf78ff82b98a13f3b1009c607fc7fdbd2
nicacioneto <nicacionetobsb@hotmail.com>
2016-04-16 19:33:31 -0300

    Create unit Test and add JUnit in the project

--- src/test/java/br/pro/delfino/drogaria/util/HibernateUtilTest.java
+++ src/test/java/br/pro/delfino/drogaria/util/HibernateUtilTest.java
@@ -1,11 +1,11 @@
-package br.pro.delfino.main;
+package br.pro.delfino.drogaria.util;
 
 import org.hibernate.Session;
-
-import br.pro.delfino.drogaria.util.HibernateUtil;
+import org.junit.Test;
 
 public class HibernateUtilTest {
-	public static void main(String[] args) {
+	@Test
+	public void conectar(){
 		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
 		sessao.close();
 		HibernateUtil.getFabricaDeSessoes().close();
