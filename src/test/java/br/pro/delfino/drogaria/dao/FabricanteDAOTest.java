b0cc788215c1f8a7e9b4a74ad513dd44d2bfaeee
nicacioneto <nicacionetobsb@hotmail.com>
2016-05-18 09:25:26 -0300

    Add Unit Test

--- src/test/java/br/pro/delfino/drogaria/dao/FabricanteDAOTest.java
+++ src/test/java/br/pro/delfino/drogaria/dao/FabricanteDAOTest.java
@@ -1,18 +1,33 @@
 package br.pro.delfino.drogaria.dao;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
+import br.pro.delfino.drogaria.domain.Estado;
 import br.pro.delfino.drogaria.domain.Fabricante;
 
 public class FabricanteDAOTest {
 
+	@Ignore
 	@Test
-	public void Salvar() {
-		Fabricante fabricante =new Fabricante();
+	public void salvar() {
+		Fabricante fabricante = new Fabricante();
 		fabricante.setDescricao("Fabricante e comerciante de medicamentos LTDA");
 		FabricanteDAO fabricanteDao = new FabricanteDAO();
 		fabricanteDao.salvar(fabricante);
-		
+
+	}
+
+	@Test
+	public void listar() {
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		List<Fabricante> resultado = fabricanteDAO.listar();
+		System.out.println("Total de Registros Encontrados: " + resultado.size());
+		for (Fabricante fabricante : resultado) {
+			System.out.println(fabricante.getCodigo() + "-" + fabricante.getDescricao());
+		}
 	}
 
 }
