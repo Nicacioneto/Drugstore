172ef2fc56a54273399698bab13c2d27fbc52a6e
Nicacioneto <nicacionetobsb@hotmail.com>
2017-08-24 23:09:14 -0300

    Update project dependencies

---
+++ src/main/java/br/pro/delfino/main/HibernateUtilTest.java
@@ -0,0 +1,12 @@
+package br.pro.delfino.main;
+
+import org.hibernate.Session;
+
+import br.pro.delfino.drogaria.util.HibernateUtil;
+
+public class HibernateUtilTest {
+	public static void main(String[] args) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		sessao.close();
+	}
+}
